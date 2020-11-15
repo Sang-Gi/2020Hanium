88537f9fdc6c008503fad30a1fdd4f0309fe9661
hyh971012 <hyh971012@naver.com>
2020-11-16 01:28:06 +0900

    11-16 1st test GPS Slimtan

--- app/src/main/java/com/example/master_piece/MainActivity.java
+++ app/src/main/java/com/example/master_piece/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.master_piece;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton gpsset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gpsset = (ImageButton) findViewById(R.id.gpsset);
+
+        gpsset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, GpsActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
+
+
+
     }
 }
